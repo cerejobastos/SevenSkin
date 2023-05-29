ee94ba8faaa0876c76db403f200f9f2281fe6cdf
André Filipe Cerejo Jesus Bastos <andre.bastos@zap.co.ao>
2023-05-29 19:25:19 +0100

    added proxy

--- src/Actions/HeartRate.Actions.js
+++ src/Actions/HeartRate.Actions.js
@@ -2,7 +2,7 @@ import { setHeartRateData } from "../Reducers/HeartRate.Reducer";
 
 export const fetchHeartRateData = () => {
     return async dispatch => {
-        const response = await fetch("heartrate");
+        const response = await fetch("/heartrate");
         const data = await response.json();
         dispatch(setHeartRateData(data));
     }
